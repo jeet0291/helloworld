2b0e79f57569942c347b8da4b849cbe26feed94e
jeet <jitendra.chandora@appinventiv.com>
2020-04-28 06:11:04 +0530

    game over

--- app.js
+++ app.js
@@ -4,10 +4,18 @@ var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 
-var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
+var server = require('./bin/www');
+const socketIO = require('socket.io');
+const io = socketIO(server);
+
+
+// var indexRouter = require('./routes/index');
+// var usersRouter = require('./routes/users');
 
 var app = express();
+
+
+console.log("Socet worng ..........");
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
@@ -19,8 +27,15 @@ app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.use('/', indexRouter);
-app.use('/users', usersRouter);
+// app.use('/', indexRouter);
+// app.use('/users', usersRouter);
+
+
+app.get('/', (request, response) => {
+  response.sendFile(path.join(__dirname, '/public/index.html'));
+});
+
+
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
@@ -37,5 +52,7 @@ app.use(function(err, req, res, next) {
   res.status(err.status || 500);
   res.render('error');
 });
+
+
 
 module.exports = app;
---
+++ public/javascripts/game.js
@@ -0,0 +1,91 @@
+'use strict';
+
+const socket = io();
+let player = {};
+
+socket.on('connect', () => {
+    console.log(socket.id);  
+
+function gameStart(){
+    socket.emit('game-start', { nickname: $("#nickname").val() });
+    $("#start-screen").hide();
+    $("#lobby-screen").show();
+    $("#scoreBoard").hide();
+}
+$("#start-button").on('click', gameStart);
+
+
+function fireToEnemy(){
+    const player = {"socketId": socket.id};
+    console.log("player",player);  
+    socket.emit('shoot', player);
+}
+$("#fire-button").on('click', fireToEnemy);
+
+
+function makeUL(players) {
+    var list;
+    list = document.getElementById('lobby-players');
+    if(!list){
+        list = document.createElement('ul');
+        list.setAttribute("id", "lobby-players");
+    }
+    for(var i = 0; i < Object.keys(players).length; i++) {
+        var item = document.createElement('li');
+        item.appendChild(document.createTextNode(Object.values(players)[i].nickname+" Fired "));
+        if(Object.values(players)[i].fire){
+            item.appendChild(document.createTextNode(Object.values(players)[i].fire+" Times"));
+        }
+        list.appendChild(item);
+    }
+    return list;
+}
+
+
+socket.on('timer',function(timerData) {
+    $('#seconds').html("Game will start in "+timerData.secondLeft+" Seconds");
+});
+
+
+socket.on('gameTime',function(timerData) {
+    $('#game-time').html("Game will end in "+timerData.secondLeft+" Seconds");
+});
+
+socket.on('playzoneStart',function(timerData) {
+    console.log("######## let's play the match ############",timerData);
+    $("#start-screen").hide();
+    $("#lobby-screen").hide();
+    $("#play-zone").show();
+    $("#scoreBoard").hide();
+});
+
+socket.on('connectToRoom',function(players) {
+    var myList = document.getElementById('lobby-players');
+    if(myList){
+        myList.innerHTML = '';
+    }
+    document.getElementById('lobby-screen').appendChild(makeUL(
+        players
+    ));
+ });
+
+ socket.on('scoreBoard',function(players) {
+    $("#start-screen").hide();
+    $("#lobby-screen").show();
+    $("#play-zone").hide();
+    var myList = document.getElementById('lobby-players');
+    if(myList){
+        myList.innerHTML = '';
+    }
+    document.getElementById('lobby-screen').appendChild(makeUL(
+        players
+    ));
+ });
+
+
+// io.to(this.socketId).emit('dead');
+socket.on('dead', () => {
+    $("#start-screen").show();
+});
+
+});
